dc33584829a28274a30d168233007037948763a1
ICT74 <inmaculada.concellon@grupoica.com>
2017-10-24 16:55:23 +0200

    hola mundo

---
+++ 01_Inicio/hola-mundo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Curso de Angular';
+  autor = 'ICT';
+  empresa = 'ICA' ;
+}
